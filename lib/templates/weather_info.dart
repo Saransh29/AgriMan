54008d6c3ca5fa65754abdb49f0d592364a96cfe
Saransh29 <saranshbibiyan234@gmail.com>
2022-11-22 23:49:28 +0530

    added theme

--- lib/templates/weather_info.dart
+++ lib/templates/weather_info.dart
@@ -1,3 +1,4 @@
+import '../theme.dart';
 import 'data_service.dart';
 import 'package:flutter/material.dart';
 
@@ -17,37 +18,39 @@ class _weatherState extends State<weather> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        color: primColors.back,
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (_response != null)
-              Column(
-                children: [
-                  Image.network(_response!.iconUrl),
-                  Text(
-                    '${_response!.tempInfo!.temperature}°',
-                    style: TextStyle(fontSize: 40),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (_response != null)
+                  Column(
+                    children: [
+                      Image.network(_response!.iconUrl),
+                      Text(
+                        '${_response!.tempInfo!.temperature}°',
+                        style: TextStyle(fontSize: 40),
+                      ),
+                      Text(_response!.weatherInfo!.description)
+                    ],
                   ),
-                  Text(_response!.weatherInfo!.description)
-                ],
-              ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 50),
-              child: SizedBox(
-                width: 150,
-                child: TextField(
-                    controller: _cityTextController,
-                    decoration: InputDecoration(labelText: 'City'),
-                    textAlign: TextAlign.center),
-              ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 50),
+                  child: SizedBox(
+                    width: 150,
+                    child: TextField(
+                        controller: _cityTextController,
+                        decoration: InputDecoration(labelText: 'City'),
+                        textAlign: TextAlign.center),
+                  ),
+                ),
+                ElevatedButton(onPressed: _search, child: Text('Search'))
+              ],
             ),
-            ElevatedButton(onPressed: _search, child: Text('Search'))
-          ],
-        ),
-      ),
-    ));
+          ),
+        ));
   }
 
   void _search() async {
